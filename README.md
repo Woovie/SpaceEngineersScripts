c912c83aa24d05d4c2abeede10bc0a6fc0ea253c
Woovie <Woovie>
2018-06-01 18:30:37 -0500

    Added group features to se-rgb.cs. Updated README.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Space Engineers Scripts
 This is a small repository of scripts I've written for Space Engineers programming blocks
 - se-rgb.cs
-  - This is a script for doing RGB color patterns for all lights on a ship. Edit adjustBy to modify speed. This triggers every game tick. Currently it's pretty fast.
+  - This is a script for doing RGB color patterns with lights on a ship.
+  - You need to make two lines in the Custom Data of the programming block this code is used in. Line 1 is your groups, line 2 is your speed.
+    - Line 1 example: ;lights1;lights2;lights3
+    - Line 2 example: 1
+  - Line 1 is a value separated field. First character defines your separation variable.
+  - Line 2 is a value between 0 and 255 representing speed.
--- se-rgb.cs
+++ se-rgb.cs
@@ -1,54 +1,64 @@
 public Program()
 {
     Runtime.UpdateFrequency = UpdateFrequency.Update1;
-    List<IMyTerminalBlock> v0 = new List<IMyTerminalBlock>();
-    for(int i = 0; i < v0.Count; i++) {
-        v0[i].SetValueColor("Color", new Color(255,0,0, 255));
-    }
 }
 void Main(string argument)
 {
     string ERR_TXT = "";
-    List<IMyTerminalBlock> v0 = new List<IMyTerminalBlock>();
-    GridTerminalSystem.GetBlocksOfType<IMyLightingBlock>(v0, filterThis);
-    if(v0.Count == 0) {
-        ERR_TXT += "no Lighting Block blocks found\n";
+    string[] variables = Me.CustomData.Split();
+    string[] groups = variables[0].Split(variables[0][0]);
+    int timeScale = Int32.Parse(variables[1]);
+    if (groups.Count() == 0) {
+        ERR_TXT += "Groups total is 0, please check your Custom Data values.\n";
+    }
+    if (timeScale > 255 | timeScale < 1) {
+        ERR_TXT += "Time scale is too low, please increase or decrease your time scale to be between 1 and 255, integers only.\n";
+    }
+    foreach (string group in groups) {
+        if (group.Length > 0) {
+            List<IMyTerminalBlock> lightBlocks = new List<IMyTerminalBlock>();
+            if(GridTerminalSystem.GetBlockGroupWithName(group) != null) {
+                GridTerminalSystem.GetBlockGroupWithName(group).GetBlocksOfType<IMyLightingBlock>(lightBlocks, filterThis);
+                            if (lightBlocks.Count == 0) {
+                ERR_TXT += "No lighting blocks found in group "+group+"\n";
+                } else if (lightBlocks.Count > 0) {//Actual lighting loop
+                    Color lightColor = lightBlocks[0].GetValueColor("Color");
+                    int r = lightColor.R;
+                    int g = lightColor.G;
+                    int b = lightColor.B;
+                    if(r == 255 & g >= 0 & g < 255 & b == 0){
+                        g=g+timeScale;
+                    }
+                    if(r <= 255 & r > 0 & g == 255 & b == 0){
+                        r=r-timeScale;
+                    }
+                    if(r == 0 & g == 255 & b >= 0 & b < 255){
+                        b=b+timeScale;
+                    }
+                    if(r == 0 & g <= 255 & g > 0 & b == 255){
+                        g=g-timeScale;
+                    }
+                    if(r >= 0 & r < 255 & g == 0 & b == 255){
+                        r=r+timeScale;
+                    }
+                    if(r == 255 & g == 0 & b <= 255 & b > 0){
+                        b=b-timeScale;
+                    }
+                    for(int i = 0; i < lightBlocks.Count; i++) {
+                        lightBlocks[i].SetValueColor("Color", new Color(r, g, b, 255));
+                    }
+                }
+            } else {
+                ERR_TXT += "Group "+group+" not found.\n";
+            }
+        }
     }
     if(ERR_TXT != "") {
-        Echo("Script Errors:\n"+ERR_TXT+"(make sure block ownership is set correctly)");
+        Echo("Script Errors:\n"+ERR_TXT+"\n(make sure block ownership is set correctly)");
         return;
-    } else {
-        Echo("");
-    }
-    if (v0.Count > 0) {
-        Color lightColor = v0[0].GetValueColor("Color");
-        int r = lightColor.R;
-        int g = lightColor.G;
-        int b = lightColor.B;
-        int adjustBy = 8;
-        if(r == 255 & g >= 0 & g < 255 & b == 0){
-            g=g+adjustBy;
-        }
-        if(r <= 255 & r > 0 & g == 255 & b == 0){
-            r=r-adjustBy;
-        }
-        if(r == 0 & g == 255 & b >= 0 & b < 255){
-            b=b+adjustBy;
-        }
-        if(r == 0 & g <= 255 & g > 0 & b == 255){
-            g=g-adjustBy;
-        }
-        if(r >= 0 & r < 255 & g == 0 & b == 255){
-            r=r+adjustBy;
-        }
-        if(r == 255 & g == 0 & b <= 255 & b > 0){
-            b=b-adjustBy;
-        }
-        for(int i = 0; i < v0.Count; i++) {
-            v0[i].SetValueColor("Color", new Color(r, g, b, 255));
-        }
     }
 }
+
 bool filterThis(IMyTerminalBlock block) {
     return block.CubeGrid == Me.CubeGrid;
 }
